1801c9639485f1e6e09ed11d8d6b19c0f52c5d3d
ArsHin <FatemMaqu@gmail.com>
2026-08-18 14:58:44 +0330

    Add: longest-palindromic-substring - submission-5

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-5.py
@@ -0,0 +1,34 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def calc_diff(r1: Tuple[int, int], r2: Tuple[int, int]):
+            d1, d2 = r1[1] - r1[0], r2[1] - r2[0]
+            if d2 > d1:
+                return r2
+            return r1
+        res = (0, 0)
+        for i in range(0, len(s)):
+            l = i - 1 if i > 0 else 0
+            r = i + 1 if i < len(s) - 1 else i
+            
+            while l >= 0 and r < len(s):
+                if s[l] != s[r]:
+                    break
+                res = calc_diff(res, (l, r))
+                l -= 1
+                r += 1
+
+            l = i - 1 if i > 0 else 0
+            r = i + 1 if i < len(s) - 1 else i
+            if s[i] == s[r]:
+                res = calc_diff(res, (i, r))
+                r += 1
+            while l >= 0 and r < len(s):
+                if s[l] != s[r]:
+                    break
+                res = calc_diff(res, (l, r))
+                l -= 1
+                r += 1
+                
+
+        return s[res[0]:res[1]+1]
+        
