1da35522be0eabb453e7d2fbf75148ac673f6a65
ArsHin <FatemMaqu@gmail.com>
2026-08-27 01:15:45 +0330

    Add: extra-characters-in-a-string - submission-15

---
+++ Data Structures & Algorithms/extra-characters-in-a-string/submission-15.py
@@ -0,0 +1,37 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        root = TrieNode()
+        for w in dictionary:
+            node = root
+            for c in w:
+                if node.children.get(c) is None:
+                    node.children[c] = TrieNode()
+                node = node.children[c]
+            node.is_end = True
+        
+        dp = {}
+        def backtrack(i: int = 0, node: TrieNode = root, count: int = 1):
+            if i >= len(s):
+                return count - 1
+
+            c = s[i]
+            if count == 1 and dp.get(i) is not None:
+                return dp[i]
+
+            if c not in node.children:
+                res = count + backtrack(i+1, root, 1)
+            else:
+                node = node.children[c]
+                res = min(backtrack(i+1, node, count+1), backtrack(i+1, root, 1) if node.is_end else count + backtrack(i+1, root, 1))
+            if count == 1:
+                dp[i] = res
+            return res
+        
+        return backtrack()
+            
+        
