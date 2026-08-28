856c41e95779c11d17deacb2f6a76a0cef4271b2
ArsHin <FatemMaqu@gmail.com>
2026-08-28 18:37:21 +0330

    Add: jump-game-ii - submission-4

---
+++ Data Structures & Algorithms/jump-game-ii/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums) <= 1:
+            return 0
+        jumps = 0
+        mi = 0  # max index
+        mi_candidate = 0
+        i = 0
+        while i < len(nums) - 1:
+            if nums[mi] + mi >= len(nums) - 1:
+                jumps += 1
+                break
+            i += 1
+            if i - (nums[mi] + mi) == 0:
+                if (nums[mi_candidate] + mi_candidate) - i > nums[i]:
+                    mi = mi_candidate
+                else:
+                    mi = i
+                jumps += 1
+            elif (nums[mi] + mi) - i < nums[i]:
+                mi_candidate = i
+        return jumps
+        
