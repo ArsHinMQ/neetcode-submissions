b11fc4fa4047c89e61a3282f6652ce01b6892cca
ArsHin <FatemMaqu@gmail.com>
2026-09-12 12:38:50 +0330

    Add: word-ladder - submission-1

---
+++ Data Structures & Algorithms/word-ladder/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        dictionary = set(wordList)
+        if endWord not in dictionary:
+            return 0
+        if len(endWord) != len(beginWord):
+            return 0
+
+        for chunk in dictionary:
+            if len(chunk) != len(beginWord):
+                dictionary.remove(chunk)
+
+        
+        queue = deque([(endWord, 1)])
+        table = defaultdict(set)
+        while queue:
+            chunk, count = queue.popleft()
+            for i in range(len(chunk)):
+                for rc in "abcdefghijklmnopkrstuvwxyz":
+                    nchunk = chunk[:i] + rc + chunk[i+1:]
+                    if nchunk == beginWord:
+                        return count + 1
+                    if nchunk not in dictionary:
+                        print(nchunk, "not in dict")
+                        continue
+                    elif nchunk == chunk:
+                        continue
+                    elif nchunk in table[chunk]:
+                        continue
+                    elif chunk in table[nchunk]:
+                        continue
+                    table[chunk].add(nchunk)
+                    table[nchunk].add(chunk)
+                    queue.append((nchunk, count+1))
+        return 0
+
