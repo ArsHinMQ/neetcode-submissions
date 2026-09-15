9812d62a2680f0ed97afa1d5b8fc6369b3b0271f
ArsHin <FatemMaqu@gmail.com>
2026-09-15 12:35:34 +0330

    Add: longest-common-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        chars1 = defaultdict(list)
+        chars2 = defaultdict(list)
+        for i, c in enumerate(text1):
+            chars1[c].append(i)
+        for i, c in enumerate(text2):
+            chars2[c].append(i)
+        def backtrack(i: int = 0, j: int = 0):
+            if i >= len(text1) or j >= len(text2):
+                return 0
+            c1, c2 = text1[i], text2[j]
+            if c1 == c2:
+                return 1 + backtrack(i+1, j+1)
+            r1, r2 = 0, 0
+            if chars2[c1]:
+                for nj in chars2[c1]:
+                    if nj < j:
+                        continue
+                    r1 = backtrack(i, nj)
+                    break
+            if chars1[c2]:
+                for ni in chars1[c2]:
+                    if ni < i:
+                        continue
+                    r2 = backtrack(ni, j)
+                    break
+
+            return max(r1, r2, backtrack(i+1, j+1))
+        return backtrack()
+        
