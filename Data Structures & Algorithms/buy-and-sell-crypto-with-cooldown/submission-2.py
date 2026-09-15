31cbb165faa5682819818c543e36ed6997194f00
ArsHin <FatemMaqu@gmail.com>
2026-09-15 20:19:30 +0330

    Add: buy-and-sell-crypto-with-cooldown - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = {}
+        def backtrack(i: int = 0, buying: bool = True):
+            if i >= len(prices):
+                return 0
+            if dp.get((i, buying)) is not None:
+                return dp[(i, buying)]
+            cooldown = backtrack(i+1, buying)
+            if buying:
+                dp[(i, buying)] = max(-prices[i] + backtrack(i+1, not buying), cooldown)
+            else:
+                dp[(i, buying)] = max(prices[i] + backtrack(i+2, not buying), cooldown)
+            return dp[(i, buying)]
+
+        return backtrack()
