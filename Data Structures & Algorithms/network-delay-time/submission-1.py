342f3e0ad4875319faa34b91b024496124c29cb7
ArsHin <FatemMaqu@gmail.com>
2026-09-19 08:28:07 +0330

    Add: network-delay-time - submission-1

---
+++ Data Structures & Algorithms/network-delay-time/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        adj = defaultdict(set)
+        for ui, vi, ti in times:
+            adj[ui].add((vi, ti))
+
+        visited = set()
+        queue = deque([(k, 0)])
+        counter = 0
+        while queue:
+            length = len(queue)
+            while length > 0:
+                node, time = queue.popleft()
+                length -= 1
+                if node in visited:
+                    continue
+                if time > 0:
+                    queue.append((node, time-1))
+                    continue
+                visited.add(node)
+                for ngh, t in adj[node]:
+                    queue.append((ngh, t-1))
+            if len(visited) == n:
+                return counter
+            counter += 1
+
+        if len(visited) == n:
+            return counter
+        return -1
