a81e86583a1aeb6b97e6997b09e88d4c4198efb0
ArsHin <FatemMaqu@gmail.com>
2026-09-22 15:25:13 +0330

    Add: single-threaded-cpu - submission-0

---
+++ Data Structures & Algorithms/single-threaded-cpu/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        mappings = defaultdict(list)
+        start_times = set()
+        for i, (te, tp) in enumerate(tasks):
+            mappings[te].append((tp, i))
+            start_times.add(te)
+
+        start_times = list(start_times)
+        heapq.heapify(start_times)
+
+        t = 1
+        result = []
+        heap = []
+        while len(result) != len(tasks):
+            while start_times and start_times[0] <= t:
+                te = heapq.heappop(start_times)
+                for tp, i in mappings[te]:
+                    heapq.heappush(heap, (tp, i))
+            if not heap:
+                t += 1
+                continue
+            tp, i = heapq.heappop(heap)
+            result.append(i)
+            t += tp
+
+        return result
+
+        
+
+        
+        
